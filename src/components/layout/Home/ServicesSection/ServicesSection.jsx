e631fa552612db9865a00b9ac402dd69d020898a
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2023-12-27 17:27:40 -0800

    error debugging Error: `'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.

--- src/components/layout/Home/ServicesSection/ServicesSection.jsx
+++ src/components/layout/Home/ServicesSection/ServicesSection.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import { RiCodeSSlashFill } from "react-icons/ri";
 import PlainCard from '@/components/shared/PlainCard/PlainCard'
--- src/components/layout/Home/Testamonials/Testamonials.jsx
+++ src/components/layout/Home/Testamonials/Testamonials.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import InfoCard from "@/components/shared/InfoCard/InfoCard";
 
