158b32ec27656758a3ea029f545ebd8d8bd752ff
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2024-01-05 03:43:44 -0800

    error resolved in vercel deployment

--- src/components/layout/Auth/Auth.js
+++ src/components/layout/Auth/Auth.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+
 import React, { Fragment, Suspense, memo, useState } from "react";
 import { useSnapshot } from "valtio";
 import axios from "axios";
