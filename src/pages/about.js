f4336957358f5144bcd64ebef3d3461328e3fa14
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2024-01-27 00:40:16 +0500

    creation of new page about us

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import About from "@/components/layout/About/About";
 
 const about = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <React.Fragment>
+      <About />
+    </React.Fragment>
+  );
+};
 
-export default about
+export default about;
